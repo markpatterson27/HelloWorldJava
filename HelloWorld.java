07ac2db8b0ae6e0f7b90bff2d269f4cf350e8e4f
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-06-15 10:20:50 +0100

    initial commit. added java file and README

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+// class that outputs 'hello world' on the command line.
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World Java
+
+Simple Java class that outputs 'hello world' on the command line.
